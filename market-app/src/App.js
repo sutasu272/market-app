2163ba2e5eec823ae8c096e4d77060d0d578e6b4
sutasu272 <ost1902@gmail.com>
2024-09-27 11:25:02 +0500

    first commit

---
+++ market-app/src/App.js
@@ -0,0 +1,13 @@
+import "./assets/css/style.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ market-app/src/components/MarketList.jsx
@@ -0,0 +1,10 @@
+import MarketCard from "./MarketCard";
+function MarketList() {
+    return (
+        <div class="game-consoles">
+            <MarketCard  title="PlayStation 5 Pro" category="Игровые приставки" price="399 990₸"/>
+        </div>
+    );
+}
+
+export default MarketList;
